22543a686672cbf715ff802d03f363eed911b17f
TomWija <tomwi@brandwatch.com>
2019-07-04 11:02:05 +0100

    feat: set up initial apps

---
+++ storybook-experiment/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@brandwatch/axiom-components';
+
+ReactDOM.render(
+    <Button>
+        Hello
+    </Button>,
+    document.getElementById('root')
+);
