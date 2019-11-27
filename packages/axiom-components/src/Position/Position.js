96cfb7e4b24ee8616178ff86002be4355b057e58
laurie <laurie@brandwatch.com>
2019-11-27 11:25:59 +0000

    chore(popper.js): Updates Position component to use React Popper

--- packages/axiom-components/src/Position/Position.js
+++ packages/axiom-components/src/Position/Position.js
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
 import React, { Component, cloneElement } from 'react';
-import ReactDOM from 'react-dom';
-import popperJS from 'popper.js';
 import omit from 'lodash.omit';
+import { Manager, Reference, Popper } from 'react-popper';
 import classnames from 'classnames';
 import { findComponent } from '@brandwatch/axiom-utils';
 import Portal from '../Portal/Portal';
 import { PositionSourceRef } from './PositionSource';
 import { PositionTargetRef } from './PositionTarget';
-import { placementToPosition, positionToPlacement, getPlacementFlipOrder } from './_utils';
+import {
+  positionToPlacement,
+  getPlacementFlipOrder,
+  placementToPosition,
+} from './_utils';
 import './Position.css';
 
 /* eslint-disable react/no-find-dom-node */
@@ -26,10 +29,7 @@ export default class Position extends Component {
      * only PositionSource and PositionTarget or just a PositionSource
      * in the case of a given reference!
      */
-    children: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.array,
-    ]).isRequired,
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]).isRequired,
     /** Class name to be appended to the element */
     className: PropTypes.string,
     /**
@@ -48,11 +48,6 @@ export default class Position extends Component {
      * function is called when clicked.
      */
     onMaskClick: PropTypes.func,
-    /**
-     * Optional handler that is called, with the new position, when PositionSource
-     * has been positioned.
-     */
-    onPositionChange: PropTypes.func,
     /**
      * Controls the starting position around PositionTarget in which the
      * PositionSource will attempt to be placed. If that position is not available
@@ -77,150 +72,95 @@ export default class Position extends Component {
     flip: 'clockwise',
     offset: 'middle',
     position: 'top',
-    showArrow:  false,
+    showArrow: false,
   };
 
-  constructor(props) {
-    super(props);
-    this.handleOnCreate = this.handleOnCreate.bind(this);
-    this.handleOnUpdate = this.handleOnUpdate.bind(this);
-    this.state = {
-      placement: positionToPlacement(props.position, props.offset),
-    };
-  }
-
-  componentDidMount() {
-    if (this._content) {
-      this._popper = this.createPopper();
-    }
-  }
-
-  componentDidUpdate() {
-    const { enabled, isVisible, position, offset } = this.props;
-
-    if (enabled && isVisible) {
-      if (!this._popper) {
-        this._popper = this.createPopper();
-      } else {
-        this._popper.options.placement = positionToPlacement(position, offset);
-        this._popper.update();
-      }
-    } else if (this._popper) {
-      this.destroyPopper();
-    }
-  }
-
-  componentWillUnmount() {
-    this.destroyPopper();
-  }
-
-  createPopper() {
-    const { boundariesElement, flip, showArrow, reference } = this.props;
-    const { placement } = this.state;
-
-    return new popperJS(reference || this._target, this._content, {
-      onCreate: this.handleOnCreate,
-      onUpdate: this.handleOnUpdate,
-      placement,
-      modifiers: {
-        arrow: {
-          enabled: showArrow,
-          element: this._arrow,
-        },
-        flip: {
-          behavior: getPlacementFlipOrder(placement, flip),
-        },
-        inner: {
-          enabled: false,
-        },
-        offset: {
-          enabled: false,
-        },
-        preventOverflow: {
-          boundariesElement,
-        },
-      },
-    });
-  }
-
-  destroyPopper() {
-    if (this._popper) {
-      this._popper.destroy();
-    }
-
-    delete this._popper;
-    delete this._arrow;
-    delete this._content;
-
-    const placement = positionToPlacement(this.props.position, this.props.offset);
-    if (placement !== this.state.placement) {
-      this.setState({ placement });
-    }
-  }
-
-  handleOnCreate(popper) {
-    const { onPositionChange } = this.props;
-
-    this.handleOnUpdate(popper);
-
-    if (onPositionChange) {
-      onPositionChange(popper.placement);
-    }
-  }
-
-  handleOnUpdate({ placement }) {
-    const { onPositionChange } = this.props;
-    const { placement: statePlacement } = this.state;
-
-    if (statePlacement !== placement) {
-      this.setState({ placement });
-
-      if (onPositionChange) {
-        onPositionChange(placement);
-      }
-    }
-  }
-
   render() {
-    const { children, className, enabled, isVisible, onMaskClick, showArrow, reference, ...rest } = this.props;
-    const { placement } = this.state;
-    const [ position ] = placementToPosition(placement);
+    const {
+      boundariesElement,
+      children,
+      className,
+      enabled,
+      isVisible,
+      flip,
+      onMaskClick,
+      reference,
+      showArrow,
+      position: initialPosition,
+      offset,
+      ...rest
+    } = this.props;
+    const classes = classnames(
+      'ax-position',
+      {
+        'ax-position--arrow': showArrow,
+      },
+      className
+    );
 
-    const classes = classnames('ax-position', {
-      'ax-position--arrow': showArrow,
-    }, className);
+    const props = omit(rest, ['flip', 'offset', 'onPositionChange']);
 
-    const props = omit(rest, [
-      'flip',
-      'offset',
-      'position',
-      'onPositionChange',
-    ]);
+    const placement = positionToPlacement(initialPosition, offset);
 
-    return [
-      !reference && cloneElement(findComponent(children, PositionTargetRef), {
-        ref: (target) => this._target = ReactDOM.findDOMNode(target),
-      }),
-      enabled && isVisible ? (
-        <Portal { ...props } key="portal">
-          <div>
-            { onMaskClick && (
-              <div className="ax-position__mask" onClick={ onMaskClick } />
-            ) }
+    return (
+      <Manager>
+        { !reference && (
+          <Reference>
+            { ({ ref }) =>
+              cloneElement(findComponent(children, PositionTargetRef), {
+                baseRef: ref,
+                inputRef: ref,
+              })
+            }
+          </Reference>
+        ) }
+        { enabled && isVisible && <Portal { ...props } key="portal">
+          <Popper
+              modifiers={ {
+                preventOverflow: {
+                  boundariesElement,
+                },
+                flip: {
+                  behavior: getPlacementFlipOrder(placement, flip),
+                },
+              } }
+              placement={ placement }
+              referenceElement={ reference }>
+            {
+                ({ placement, ref, style, arrowProps }) => {
+                  let position = initialPosition;
+                  if (placement) {
+                    position = placementToPosition(placement)[0];
+                  }
 
-            <div className={ classes } ref={ (el) => this._content = el }>
-              {
-                cloneElement(findComponent(children, PositionSourceRef), {
-                  arrowRef: showArrow
-                    ? (arrow) => this._arrow = ReactDOM.findDOMNode(arrow)
-                    : undefined,
-                  position,
-                })
+                  return (
+                    <div
+                        className={ classes }
+                        data-popper-placement={ placement }
+                        ref={ ref }
+                        style={ style }>
+                      <div>
+                        { onMaskClick && (
+                          <div
+                              className="ax-position__mask"
+                              onClick={ onMaskClick }/>
+                        ) }
+                        { cloneElement(
+                          findComponent(children, PositionSourceRef),
+                          {
+                            arrowRef: showArrow ? arrowProps.ref : undefined,
+                            position,
+                            arrowStyle: arrowProps.style,
+                          }
+                        ) }
+                      </div>
+                    </div>
+                  );
+                }
               }
-            </div>
-          </div>
-        </Portal>
-      ) : null,
-    ];
+          </Popper>
+        </Portal> }
+      </Manager>
+    );
   }
 }
