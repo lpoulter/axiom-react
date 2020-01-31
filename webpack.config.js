80738c9984ae2c39038c0243168bebb93fb78b0e
laurie <laurie@brandwatch.com>
2020-01-31 17:00:18 +0000

    build(babel): Update to babel 7.
    Actually and correctly increases build size see https://github.com/babel/babel/issues/8401
    Build size = 1.43mb

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: [
-    'babel-polyfill',
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:4000',
-    'webpack/hot/only-dev-server',
-    './site/index.js',
+    "@babel/polyfill",
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:4000",
+    "webpack/hot/only-dev-server",
+    "./site/index.js"
   ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: ['babel-loader'],
-      include: [
-        /packages/,
-        /site/,
-        /node_modules\/get-own-enumerable-property-symbols/,
-        /node_modules\/stringify-object/,
-      ],
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        include: [
+          /packages/,
+          /site/,
+          /node_modules\/get-own-enumerable-property-symbols/,
+          /node_modules\/stringify-object/
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      }
+    ]
   },
   output: {
-    filename: 'bundle.js',
-    publicPath: '/',
+    filename: "bundle.js",
+    publicPath: "/"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
-      template: './site/index.ejs',
-      basename: '/',
+      template: "./site/index.ejs",
+      basename: "/"
     }),
     new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development',
-    }),
+      NODE_ENV: "development"
+    })
   ],
   resolveLoader: {
-    modules: [
-      'node_modules',
-      'packages',
-    ],
+    modules: ["node_modules", "packages"]
   },
   devServer: {
-    contentBase: './site',
+    contentBase: "./site",
     historyApiFallback: true,
     hot: true,
     inline: true,
-    port: 4000,
-  },
+    port: 4000
+  }
 };
