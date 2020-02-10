750c5c9d909af5332e6a14f5ba2e19adaec24962
Eliot Stocker <estocker@brandwatch.com>
2020-02-10 14:51:04 +0000

    build: update build to generate both esm and cjs
    also update package jsons to point modules at esm build

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -3,9 +3,17 @@ set -e
 
 # Prepare
 npx lerna exec --parallel -- rm -rf dist
-npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist
 
 # Build
 yarn build:flags
-npx lerna exec --parallel -- npx babel dist -d dist --config-file $(pwd)/babel.config.js
+
+#CJS
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist
+npx lerna exec --parallel -- npx babel dist -d dist --config-file $(pwd)/babel.config.cjs.js
+
+#ESM
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist/esm
+npx lerna exec --parallel -- npx babel dist/esm -d dist/esm --config-file $(pwd)/babel.config.esm.js
+
+#CSS
 npx postcss packages/**/dist/**/*.css --config ./postcss.prod.config.js --replace --verbose
