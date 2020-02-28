a9fac378237b9df1e780bf7a344717a036ef17ce
laurie <laurie@brandwatch.com>
2020-02-28 23:48:34 +0000

    refactor: 💡 Convert DropdownContext to Function component

--- packages/axiom-components/src/Dropdown/DropdownContext.js
+++ packages/axiom-components/src/Dropdown/DropdownContext.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useContext, useRef, useEffect } from 'react';
 import omit from 'lodash.omit';
 import Context from '../Context/Context';
 import { contextMenuItemSelector } from '../Context/ContextMenuItem';
@@ -10,77 +9,57 @@ if (typeof window !== 'undefined') {
   require('element-closest');
 }
 
-const isFocusableMenuItem = (element) =>
-  element && element.hasAttribute(contextMenuItemSelector) &&
-    !element.disabled;
+const isFocusableMenuItem = element =>
+  element && element.hasAttribute(contextMenuItemSelector) && !element.disabled;
+export default function DropdownContext(props) {
+  const { maxHeight = '30rem', position = 'top', width = '14.5rem' } = props;
 
-/* eslint-disable react/no-find-dom-node */
-export default class DropdownContext extends Component {
-  static propTypes = {
-    /** React reference function for the arrow element */
-    arrowRef: PropTypes.func,
-    /** Content to be inserted in the contextual area */
-    children: PropTypes.node,
-    /** Color of the Context */
-    color: PropTypes.oneOf(['success', 'warning', 'error', 'info']),
-    /** Maximum height for the content area, exceeding this will make it scrollable */
-    maxHeight: PropTypes.string,
-    /** Position of the content area relative to the arrow */
-    position: PropTypes.oneOf(['top', 'bottom', 'right', 'left']),
-    /** Total width of the component */
-    width: PropTypes.string,
-  };
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('mousedown', handleClick);
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('mousedown', handleClick);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
-  static defaultProps = {
-    maxHeight: '30rem',
-    position: 'top',
-    width: '14.5rem',
-  };
+  const { dropdownRef: getDropdownRef, closeDropdown } = useContext(
+    DropdownReactContext
+  );
 
-  static contextType = DropdownReactContext;
+  const contextRef = useRef(null);
 
-  constructor(props) {
-    super(props);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-  }
+  function handleClick(event) {
+    const dropdownRef = getDropdownRef();
 
-  UNSAFE_componentWillMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-    document.removeEventListener('mousedown', this.handleClick);
-    document.removeEventListener('mousemove', this.handleMouseMove);
-  }
-
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClick);
-    document.addEventListener('mousemove', this.handleMouseMove);
-  }
-
-  handleClick(event) {
-    if (!this.context.dropdownRef || !this.context.closeDropdown) {
+    if (!dropdownRef || !closeDropdown) {
       return;
     }
 
-    const dropdownRef = this.context.dropdownRef();
-    if (dropdownRef && !dropdownRef.contains(event.target) && !this.el.contains(event.target)) {
-      return this.context.closeDropdown();
+    if (
+      dropdownRef &&
+      !dropdownRef.contains(event.target) &&
+      !contextRef.current.contains(event.target)
+    ) {
+      return closeDropdown();
     }
   }
 
-  getFocusedMenuItem() {
-    return this.el.querySelector(`[${contextMenuItemSelector}]:focus`);
+  function getFocusedMenuItem() {
+    return contextRef.current.querySelector(`[${contextMenuItemSelector}]:focus`);
   }
 
-  getMenuItems() {
-    return [...this.el.querySelectorAll(`[${contextMenuItemSelector}]:not(:disabled)`)];
+  function getMenuItems() {
+    return [
+      ...contextRef.current.querySelectorAll(
+        `[${contextMenuItemSelector}]:not(:disabled)`
+      ),
+    ];
   }
 
-  focusMenuItem(element) {
+  function focusMenuItem(element) {
     const menuItem = element.closest(`[${contextMenuItemSelector}]`);
 
     if (menuItem && isFocusableMenuItem(menuItem)) {
@@ -88,37 +67,37 @@ export default class DropdownContext extends Component {
     }
   }
 
-  handleMouseMove(event) {
-    this.focusMenuItem(event.target);
+  function handleMouseMove(event) {
+    focusMenuItem(event.target);
   }
 
-  handleKeyDown(event) {
+  function handleKeyDown(event) {
     switch (event.key) {
     case 'ArrowDown':
       event.preventDefault();
-      return this.handleDirectionKey('nextElementSibling', 1);
+      return handleDirectionKey('nextElementSibling', 1);
     case 'ArrowUp':
       event.preventDefault();
-      return this.handleDirectionKey('previousElementSibling', -1);
+      return handleDirectionKey('previousElementSibling', -1);
     case 'Tab':
     case 'Escape':
       event.preventDefault();
-      return this.context.closeDropdown();
+      return closeDropdown();
     }
   }
 
-  handleDirectionKey(sibling, delta) {
-    const focusedMenuItem = this.getFocusedMenuItem();
-    const siblingElement = focusedMenuItem && isFocusableMenuItem(focusedMenuItem[sibling])
-      ? focusedMenuItem[sibling]
-      : null;
-
+  function handleDirectionKey(sibling, delta) {
+    const focusedMenuItem = getFocusedMenuItem();
+    const siblingElement =
+      focusedMenuItem && isFocusableMenuItem(focusedMenuItem[sibling])
+        ? focusedMenuItem[sibling]
+        : null;
 
     if (siblingElement) {
       return siblingElement.focus();
     }
 
-    const menuItems = this.getMenuItems();
+    const menuItems = getMenuItems();
     const index = focusedMenuItem && menuItems.indexOf(focusedMenuItem);
 
     if (focusedMenuItem && menuItems[index + delta]) {
@@ -128,10 +107,27 @@ export default class DropdownContext extends Component {
     }
   }
 
-  render() {
-    return (
-      <Context { ...omit(this.props, ['focusOnOpen', 'onRequestCloseDropdown']) }
-          ref={ (el) => this.el = ReactDOM.findDOMNode(el) } />
-    );
-  }
+  return (
+    <Context
+        maxHeight={ maxHeight }
+        position={ position }
+        width={ width }
+        { ...omit(props, ['focusOnOpen', 'onRequestCloseDropdown']) }
+        baseRef={ contextRef }/>
+  );
 }
+
+DropdownContext.propTypes = {
+  /** React reference function for the arrow element */
+  arrowRef: PropTypes.func,
+  /** Content to be inserted in the contextual area */
+  children: PropTypes.node,
+  /** Color of the Context */
+  color: PropTypes.oneOf(['success', 'warning', 'error', 'info']),
+  /** Maximum height for the content area, exceeding this will make it scrollable */
+  maxHeight: PropTypes.string,
+  /** Position of the content area relative to the arrow */
+  position: PropTypes.oneOf(['top', 'bottom', 'right', 'left']),
+  /** Total width of the component */
+  width: PropTypes.string,
+};
