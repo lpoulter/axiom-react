3035721d6afa97d09203a94d70292010488da9ad
laurie <laurie@brandwatch.com>
2020-03-01 17:52:32 +0000

    fix: 🐛 Modal/Dialog shouldCloseOnEsc bug
    shouldCloseOnEsc only worked when shouldCloseOnEsc was true

--- packages/axiom-components/src/Modal/Modal.js
+++ packages/axiom-components/src/Modal/Modal.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import classnames from 'classnames';
 import Base from '../Base/Base';
 import Portal from '../Portal/Portal';
@@ -15,104 +15,66 @@ const disableScrolling = () => {
   document.body.classList.add(bodyOpenClassName);
 };
 
-export default class Modal extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    isOpen: PropTypes.bool.isRequired,
-    onOverlayClick: PropTypes.func,
-    overlayShade: PropTypes.oneOf([
-      'shade-1',
-      'shade-2',
-      'shade-3',
-      'shade-4',
-    ]),
-    overlayTheme: PropTypes.oneOf(['day', 'night']),
-    padding: PropTypes.oneOf(['x0', 'x6', 'x8', 'x12', 'x16']),
-    shouldCloseOnEsc: PropTypes.bool,
-  };
+Modal.propTypes = {
+  children: PropTypes.node,
+  closeOnOverlayClick: PropTypes.bool,
+  isOpen: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.bool,
+  overlayShade: PropTypes.oneOf(['shade-1', 'shade-2', 'shade-3', 'shade-4']),
+  overlayTheme: PropTypes.oneOf(['day', 'night']),
+  padding: PropTypes.oneOf(['x0', 'x6', 'x8', 'x12', 'x16']),
+  shouldCloseOnEsc: PropTypes.bool,
+};
 
-  static defaultProps = {
-    overlayShade: 'shade-2',
-    padding: 'x6',
-  };
+export default function Modal({
+  children,
+  isOpen,
+  onRequestClose,
+  overlayShade = 'shade-2',
+  overlayTheme,
+  closeOnOverlayClick,
+  padding = 'x6',
+  shouldCloseOnEsc,
+}) {
 
-  constructor(props) {
-    super(props);
-    this.handleEscapeKey = this.handleEscapeKey.bind(this);
-  }
-
-  componentDidMount() {
-    if (this.props.isOpen) {
-      disableScrolling();
-    }
-    if (this.props.shouldCloseOnEsc) {
-      document.body.addEventListener('keydown', this.handleEscapeKey, false);
-    }
-  }
-
-  componentWillUnmount() {
-    enableScrolling();
-    if (this.props.shouldCloseOnEsc) {
-      document.body.removeEventListener('keydown', this.handleEscapeKey, false);
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (!prevProps.isOpen && this.props.isOpen) {
-      disableScrolling();
-    } else if (prevProps.isOpen && !this.props.isOpen) {
-      enableScrolling();
-    }
-
-    if (this.props.shouldCloseOnEsc !== prevProps.shouldCloseOnEsc) {
-      this.toggleEscapeEventListener();
-    }
-  }
-
-  toggleEscapeEventListener() {
-    if (this.props.shouldCloseOnEsc) {
-      document.body.addEventListener('keydown', this.handleEscapeKey, false);
-    } else {
-      document.body.removeEventListener('keydown', this.handleEscapeKey, false);
-    }
-  }
-
-  handleEscapeKey(event) {
-    const { onOverlayClick, shouldCloseOnEsc } = this.props;
-
-    if (event.key === 'Escape' && shouldCloseOnEsc) {
-      if (onOverlayClick) {
-        onOverlayClick();
+  useEffect(() => {
+    function handleEscapeKey(event) {
+      if (event.key === 'Escape' && shouldCloseOnEsc) {
+        onRequestClose();
+        document.body.removeEventListener('keydown', handleEscapeKey, false);
       }
     }
-  }
 
-  render() {
-    const {
-      children,
-      isOpen,
-      overlayShade,
-      overlayTheme,
-      onOverlayClick,
-      padding,
-    } = this.props;
+    if (isOpen) {
+      disableScrolling();
+      if (shouldCloseOnEsc) {
+        document.body.addEventListener('keydown', handleEscapeKey, false);
+      }
+    }
 
-    const classes = classnames('ax-modal__container', `ax-modal__container--padding-${padding}`, {
+    return () => {
+      enableScrolling();
+      document.body.removeEventListener('keydown', handleEscapeKey, false);
+    };
+  }, [isOpen]);
+
+  const classes = classnames(
+    'ax-modal__container',
+    `ax-modal__container--padding-${padding}`,
+    {
       [`ax-modal__container--overlay-${overlayShade}`]: overlayShade,
-    });
+    }
+  );
 
-    return isOpen ? (
-      <Portal>
-        <Base className={ classes } theme={ overlayTheme }>
-          { onOverlayClick && (
-            <div className="ax-modal__mask" onClick={ onOverlayClick } />
-          ) }
+  return isOpen ? (
+    <Portal>
+      <Base className={ classes } theme={ overlayTheme }>
+        { closeOnOverlayClick && (
+          <div className="ax-modal__mask" onClick={ onRequestClose } />
+        ) }
 
-          <div className="ax-modal">
-            { children }
-          </div>
-        </Base>
-      </Portal>
-    ) : null;
-  }
+        <div className="ax-modal">{ children }</div>
+      </Base>
+    </Portal>
+  ) : null;
 }
