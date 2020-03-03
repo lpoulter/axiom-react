269c99ebcf962603623f81da9f2f0f539b394f92
laurie <laurie@brandwatch.com>
2020-03-03 19:33:50 +0000

    refactor wip

--- packages/axiom-components/src/Select/Select.js
+++ packages/axiom-components/src/Select/Select.js
@@ -7,74 +7,64 @@ import DropdownSource from '../Dropdown/DropdownSource';
 import DropdownTarget from '../Dropdown/DropdownTarget';
 import SelectInput from './SelectInput';
 
-export default class Select extends Component {
-  static propTypes = {
-    /**
-     * Children inside Select should contain all of and
-     * only SelectOption and SelectOptionGroup!
-     */
-    children: PropTypes.node,
-    /** Event that is fired when the input field is cleared */
-    onClear: PropTypes.func,
-    /** Invoked when the SelectMenu is closed. */
-    onRequestClose: PropTypes.func,
-    /** Invoked when the SelectMenu is opened. */
-    onRequestOpen: PropTypes.func,
-    /** Event that is fired when an option is selected */
-    onSelect: PropTypes.func,
-    /** The value of the selected option  */
-    selectedValue: PropTypes.any,
-    /** Value of the input field */
-    value: PropTypes.string.isRequired,
-  };
 
-  static defaultProps = {
-    value: '',
-    onSelect: () => {},
-  };
+export default function Select({
+  children,
+  onRequestClose,
+  onRequestOpen,
+  onSelect = () => {},
+  ...rest
+}) {
+  // static childContextTypes = {
+  //   handleSelectOption: PropTypes.func.isRequired,
+  //   selectedOptionValue: PropTypes.any,
+  // };
 
-  static childContextTypes = {
-    handleSelectOption: PropTypes.func.isRequired,
-    selectedOptionValue: PropTypes.any,
-  };
+  // getChildContext() {
+  //   return {
+  //     handleSelectOption: this.handleSelectOption,
+  //     selectedOptionValue: this.props.selectedValue,
+  //   };
+  // }
 
-  constructor(props) {
-    super(props);
-    this.handleSelectOption = this.handleSelectOption.bind(this);
+  function handleSelectOption(value) {
+    onSelect(value);
   }
 
-  getChildContext() {
-    return {
-      handleSelectOption: this.handleSelectOption,
-      selectedOptionValue: this.props.selectedValue,
-    };
-  }
+  return (
+    <Dropdown
+        enabled={ Children.count(children) > 0 }
+        flip="mirror"
+        onRequestClose={ onRequestClose }
+        onRequestOpen={ onRequestOpen }
+        position="bottom">
+      <DropdownTarget>
+        <SelectInput { ...omit(rest, ['onSelect', 'selectedValue']) } />
+      </DropdownTarget>
 
-  handleSelectOption(value) {
-    this.props.onSelect(value);
-  }
-
-  render() {
-    const { children, onRequestClose, onRequestOpen, ...props } = this.props;
-
-    return (
-      <Dropdown
-          enabled={ Children.count(children) > 0 }
-          flip="mirror"
-          onRequestClose={ onRequestClose }
-          onRequestOpen={ onRequestOpen }
-          position="bottom">
-        <DropdownTarget>
-          <SelectInput
-              { ...omit(props, ['onSelect', 'selectedValue']) } />
-        </DropdownTarget>
-
-        <DropdownSource focusOnOpen>
-          <DropdownContext>
-            { children }
-          </DropdownContext>
-        </DropdownSource>
-      </Dropdown>
-    );
-  }
+      <DropdownSource focusOnOpen>
+        <DropdownContext>{ children }</DropdownContext>
+      </DropdownSource>
+    </Dropdown>
+  );
 }
+
+Select.propTypes = {
+  /**
+   * Children inside Select should contain all of and
+   * only SelectOption and SelectOptionGroup!
+   */
+  children: PropTypes.node,
+  /** Event that is fired when the input field is cleared */
+  onClear: PropTypes.func,
+  /** Invoked when the SelectMenu is closed. */
+  onRequestClose: PropTypes.func,
+  /** Invoked when the SelectMenu is opened. */
+  onRequestOpen: PropTypes.func,
+  /** Event that is fired when an option is selected */
+  onSelect: PropTypes.func,
+  /** The value of the selected option  */
+  selectedValue: PropTypes.any,
+  /** Value of the input field */
+  value: PropTypes.string.isRequired,
+};
